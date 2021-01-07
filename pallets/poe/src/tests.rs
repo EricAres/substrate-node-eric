f83353af1dcec433d26d37af8c455893596d4439
Eric <eric.ares@outlook.com>
2021-01-07 21:16:22 +0800

    Update tests.rs

--- pallets/poe/src/tests.rs
+++ pallets/poe/src/tests.rs
@@ -4,7 +4,7 @@ use super::*;
 
 #[test]
 fn create_claim_works(){
-    new_test_ext().execute_with(execute: ||{
+    new_test_ext().execute_with(|| {
         let claim: Vec<i32> = vec![0,1];
         assert_ok!(PoeModule::create_claim(Origin::signed(1),claim.clone()));
 
@@ -15,7 +15,7 @@ fn create_claim_works(){
 
 #[test]
 fn create_claim_failed_when_claim_already_exist(){
-    new_test_ext().execute_with(execute: ||{
+    new_test_ext().execute_with(||{
         let claim: Vec<u8> = vec![0,1];
         let _=PoeMoule::create_claim(origin: Origin::signed(1),claim.clone());
     })
